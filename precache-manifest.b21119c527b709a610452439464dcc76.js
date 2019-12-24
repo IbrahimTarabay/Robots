055ed0237d22735f8abadcc02c9c1fbd7794e9c3
IbrahimTarabay <ibrahimesalah69@gmail.com>
2019-12-24 13:14:11 +0200

    Updates

---
+++ precache-manifest.b21119c527b709a610452439464dcc76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf4264a214b64ec6907a08800547ca5",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "ec9ceb350f5ccb06a44a",
+    "url": "/Robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "27f74d8d374349455ad9",
+    "url": "/Robots/static/css/main.d83236ab.chunk.css"
+  },
+  {
+    "revision": "ec9ceb350f5ccb06a44a",
+    "url": "/Robots/static/js/2.98510fdc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robots/static/js/2.98510fdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27f74d8d374349455ad9",
+    "url": "/Robots/static/js/main.78c84d3a.chunk.js"
+  },
+  {
+    "revision": "d33647ad124c3b569b6d",
+    "url": "/Robots/static/js/runtime-main.ee0d2471.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);

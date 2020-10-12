8311d05b023127920a2713736910694de7f6dde5
Ibrahim Salah <32605959+IbrahimTarabay@users.noreply.github.com>
2020-10-12 19:54:33 +0200

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -8,7 +8,7 @@ const CardList = ({robots}) =>{
       robots.map((robot,i) =>{
        return(
          <Card 
-           key={i}
+           key={i}/*key={i} because if one of these cards are deleted react won't know which one and it has to change entire dom*/
            id={robot.id}
            name={robot.name}
            email={robot.email}
